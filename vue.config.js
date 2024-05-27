d012ba5d3a4e4043f6941152df3bb1df754c0906
azamat-110 <tursunbayevazamat565@gmail.com>
2024-05-27 19:41:19 +0500

    config settings

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
+  publicPath: "/Note-app/",
   transpileDependencies: true
 })
